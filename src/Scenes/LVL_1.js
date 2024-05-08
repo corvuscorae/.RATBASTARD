08fc9b865583d5c812ce59d333600fb3e02356ce
corvuscorae <corvuscorae@gmail.com>
2024-05-08 00:07:46 -0700

    idk

--- src/Scenes/LVL_1.js
+++ src/Scenes/LVL_1.js
@@ -8,22 +8,23 @@ class LVL_1 extends Phaser.Scene {
         // player movement speeds (in pixels/tick)
         this.playerSpeed = 5;
         this.throwSpeed = 5;
+        this.my.sprite.potion = [];
+        this.maxPots = 10;
 
         // townie stuff
-        this.my.sprite.townie = [];
+        this.my.sprite.townieMob = [];
         this.maxTownies = 10;
         this.townieCooldown = 10;
         this.townieCountdown = 0;
         this.delay = 100;
+        this.towniesKilled = 0;
         this.i = -1;
 
         this.potionCooldown = 3;
         this.cooldownCounter = 0;
 
         // waves
-        this.wave_1 = true;
-        this.wave_2 = false;
-        this.wave_3 = false;
+        this.wave = 1;
     }
 
     preload() {
@@ -65,42 +66,34 @@ class LVL_1 extends Phaser.Scene {
             this, game.config.width/2, 40, "wizard", null, this.left, this.right, 5);
         my.sprite.wizard.setScale(3);
 
-        // player potions
-        my.sprite.potionGroup = this.add.group({
-            active: true,
-            defaultKey: "playerPotion",
-            maxSize: 10,
-            runChildUpdate: true
-            }
-        )
-        my.sprite.potionGroup.createMultiple({
-            classType: Potion,
-            active: false,
-            key: my.sprite.potionGroup.defaultKey,
-            repeat: my.sprite.potionGroup.maxSize-1,
-            setScale: { x:2, y:2 },
-            setXY: { x:-10, y:-10 } // starting position set offscreen
-        });
-        my.sprite.potionGroup.propertyValueSet("speed", this.throwSpeed);
-
         // townies
         this.points = [
-            20, 20,
-            80, 400,
-            300, 750
+            -100,-100,
+            0, 200,
+            600, 200,
+            600, 300,
+            0, 300,
+            0, 400,
+            600, 400,
+            600, 500,
+            0, 500,
+            0, 600,
+            600, 600,
+            600, 700,
+            300, 700,
+            300, 900
         ];
         this.curve = new Phaser.Curves.Spline(this.points); //* Creation of the Spline curve
-        while (my.sprite.townie.length < this.maxTownies) {
-            my.sprite.townie.push(
+        while (my.sprite.townieMob.length < this.maxTownies) {
+            my.sprite.townieMob.push(
                 this.add.follower(this.curve, this.curve.points[0].x, this.curve.points[0].y, "townieA")
             );
-            my.sprite.townie[my.sprite.townie.length-1].setScale(2);
-            my.sprite.townie[my.sprite.townie.length-1].setVisible(false);
-            my.sprite.townie[my.sprite.townie.length-1].rx = 8;
-            my.sprite.townie[my.sprite.townie.length-1].ry = 8;
+            my.sprite.townieMob[my.sprite.townieMob.length-1].setScale(2);
+            //my.sprite.townieMob[my.sprite.townieMob.length-1].setVisible(false);
+            my.sprite.townieMob[my.sprite.townieMob.length-1].rx = 8;
+            my.sprite.townieMob[my.sprite.townieMob.length-1].ry = 8;
         }
-        //my.sprite.townie = this.add.follower(this.curve, 200, 200, "townieA");
-        //my.sprite.townie.setScale(2);
+
         // update HTML description
         //document.getElementById('description').innerHTML = '<h2>Class Bullet.js</h2><br>A: left // D: right // Space: fire/emit // S: Next Scene'
 
@@ -112,13 +105,32 @@ class LVL_1 extends Phaser.Scene {
         this.townieCountdown--;
         
         this.fireBullets();
+        if(this.towniesKilled == 10 * this.wave){
+            this.wave++;
+            this.towniesKilled = 0;
+            this.i = -1;
+            for(let townie of my.sprite.townieMob){
+                townie.x = -100;
+                townie.y = -100;
+                townie.setVisible(true);
+            }
+        }
+        for(let townie of my.sprite.townieMob){
+            if(townie.y > 800){ 
+                this.towniesKilled++; 
+                townie.x = -100;
+                townie.y = -100;
+                townie.stopFollow();
+                //console.log("rip " + this.wave); 
+            }
+        }
         if(this.delay < 0 && this.i < this.maxTownies - 1){//} && this.i < this.maxTownies){
             this.i++;
             this.townieRunner(this.i);
             this.delay = 10;
         }
         my.sprite.wizard.update();
-        // figure out damn collisions
+
     }
 
     fireBullets(){
@@ -127,17 +139,33 @@ class LVL_1 extends Phaser.Scene {
 
         // Check for bullet being fired
         if (Phaser.Input.Keyboard.JustDown(this.space)) {
-            if (this.cooldownCounter < 0) {
-                // Get the first inactive bullet, and make it active
-                let potion = my.sprite.potionGroup.getFirstDead();
-                // bullet will be null if there are no inactive (available) bullets
-                if (potion != null) {
-                    this.cooldownCounter = this.potionCooldown;
-                    potion.makeActive();
-                    potion.x = my.sprite.wizard.x + (my.sprite.wizard.displayWidth/2.5);
-                    potion.y = my.sprite.wizard.y + (my.sprite.wizard.displayHeight/2.5);
+            if(my.sprite.potion.length < this.maxPots){
+                my.sprite.potion.push(this.add.sprite(
+                    my.sprite.wizard.x, my.sprite.wizard.y, "playerPotion")
+                );
+                my.sprite.potion[my.sprite.potion.length-1].setScale(2);
+            }
+        }
+
+        my.sprite.potion = my.sprite.potion.filter((potion) => potion.y < game.config.height + 20);
+        // COLLISION
+        // Check for collision with townies
+        for (let potion of my.sprite.potion) {
+            for (let townie of my.sprite.townieMob) {
+                if (townie.visible && this.collision(townie, potion)) {
+                    // clear out bullet -- put y offscreen, will get reaped next update
+                    console.log("hit!");
+                    potion.y = 900;
+                    townie.visible = false;
+                    townie.stopFollow();
+                    this.towniesKilled++;
                 }
             }
+        }
+
+        // Make all of the bullets move
+        for (let potion of my.sprite.potion) {
+            potion.y += this.throwSpeed;
         }
         
     }
@@ -146,12 +174,12 @@ class LVL_1 extends Phaser.Scene {
         let my = this.my;
         
         if(this.townieCountdown < 0){
-            my.sprite.townie[curr].setVisible(true);
-            my.sprite.townie[curr].startFollow({
+            //my.sprite.townieMob[curr].setVisible(true);
+            my.sprite.townieMob[curr].startFollow({
                 from: 0,
                 to: 1,
                 delay: 0,
-                duration: 5000,
+                duration: 30000 - (10000 * (this.wave - 1)), // make faster with subsequent waves
                 ease: 'Sine.easeInOut',
                 repeat: 0,
                 yoyo: false,
@@ -164,9 +192,9 @@ class LVL_1 extends Phaser.Scene {
         return;
     }
 
-    collision(a, b){
-        if(Math.abs(a.x - b.x) > (a.rx + b.rx)){ return false; }
-        if(Math.abs(a.y - b.y) > (a.ry + b.ry)){ return false; }
+    collision(a, b) {
+        if (Math.abs(a.x - b.x) > (a.displayWidth/2 + b.displayWidth/2)) return false;
+        if (Math.abs(a.y - b.y) > (a.displayHeight/2 + b.displayHeight/2)) return false;
         return true;
     }
 }
